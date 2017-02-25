377de145a75b87afdd7cc8a63e28695db27ff555
ranggarmaste <ranggarmaste@gmail.com>
2017-02-25 16:46:14 +0700

    Fix notification

--- routes/user_key.js
+++ routes/user_key.js
@@ -1,6 +1,6 @@
 const models = require('../models')
 const UserKey = models.UserKey
-const SERVER_KEY = 'AAAAUrsJw7w:APA91bEYw63CLTOAZs6nneenW3iS2TxVhEgLRAhDhK9u5LUC1J-kG7dk7j-l0mC4dOpgiNtPasSJFsTrs2kMdhAK6nNrNAicDHhenb-gt81yto-wFculDTS2UQw9iwTAE9XU-PNXoK6w'
+const request = require('request')
 
 module.exports = (router) => {
   router.get('/user_keys', (req, res) => {
@@ -54,24 +54,29 @@ module.exports = (router) => {
         username: username
       }
     }).then((user) => {
-      let key = user.key
-      sendMessageToUser(key, "You just found a new monster!")
+      let key = user.deviceKey
+      sendMessageToUser(key, 40)
+      res.json({status: "OK"})
     })
   })
 }
 
-function sendMessageToUser(deviceId, message) {
+function sendMessageToUser(deviceId, exp) {
   request({
     url: 'https://fcm.googleapis.com/fcm/send',
     method: 'POST',
     headers: {
       'Content-Type' : 'application/json',
-      'Authorization': SERVER_KEY
+      'Authorization': 'key=AAAAUrsJw7w:APA91bEYw63CLTOAZs6nneenW3iS2TxVhEgLRAhDhK9u5LUC1J-kG7dk7j-l0mC4dOpgiNtPasSJFsTrs2kMdhAK6nNrNAicDHhenb-gt81yto-wFculDTS2UQw9iwTAE9XU-PNXoK6w'
     },
     body: JSON.stringify(
       {
+        "notification": {
+          "title": "New monster",
+          "body": "You just found a new monster!"
+        },
         "data": {
-          "message": message,
+          "exp": exp,
         },
         "to" : deviceId
       }
